9b32a2caf36f8b3d1c4201c21126572a5510ec0a
m0rn <morn88@gmail.com>
2020-01-05 01:50:58 +0400

    Lesson 5

---
+++ app/src/main/java/com/m0rn/swoosh/SkillActivity.kt
@@ -0,0 +1,13 @@
+package com.m0rn.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
--- app/src/main/java/com/m0rn/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/m0rn/swoosh/WelcomeActivity.kt
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        getStartedBtn.setOnClickListener{
+        getStartedBtn.setOnClickListener {
             val leagueIntent = Intent(this, LeagueActivity::class.java)
             startActivity(leagueIntent)
         }
